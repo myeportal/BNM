f2d3845a64a47d7cfda8eb55c83699666005556f
Bryce Palichuk <bpalichu@ualberta.ca>
2022-07-01 20:31:36 -0600

    Limit file uploads to 4mb
    Next.js requires that files routed through apis can't exceed 4mb

---
+++ .history/pages/api/noop_20220701201528.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+    res.status(200).end("noop");
+}
+
+export const config = {
+    api: {
+        bodyParser: {
+            bodyLimit: '4mb',
+        },
+    },
+};
+
+// import * as bodyParser from 'body-parser';
+
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useStaticAssets(`${__dirname}/public`);
+//   // the next two lines did the trick
+//   app.use(bodyParser.json({limit: '50mb'}));
+//   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+//   app.enableCors();
+//   await app.listen(3001);
+// }
+// bootstrap();
---
+++ .history/pages/api/noop_20220701202456.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+    res.status(200).end("noop");
+}
+
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '4mb',
+        },
+        // responseLimit: false,
+    },
+};
+
+// import * as bodyParser from 'body-parser';
+
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useStaticAssets(`${__dirname}/public`);
+//   // the next two lines did the trick
+//   app.use(bodyParser.json({limit: '50mb'}));
+//   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+//   app.enableCors();
+//   await app.listen(3001);
+// }
+// bootstrap();
