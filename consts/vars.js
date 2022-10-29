68644c61f1579c1cd41ba18db7d16c7e16d1b8c8
Bryce Palichuk <bpalichu@ualberta.ca>
2022-10-28 20:06:50 -0600

    Add Polygon mainnet address

--- consts/vars.js
+++ consts/vars.js
@@ -1,8 +1,8 @@
 // V2 contract (Polygon Mainnet)
-// export const CHAIN = "Polygon";
-// export const CHAIN_ID = "0x89";
-// export const CONTRACT_ADDRESS = "0x3b8F7FFE57a2b545e5E65DdceD4484648D291d33";
-// export const STORAGE_ADDRESS = "FILL_ME_IN";"; 
+export const CHAIN = "Polygon";
+export const CHAIN_ID = "0x89";
+export const CONTRACT_ADDRESS = "0x3b8F7FFE57a2b545e5E65DdceD4484648D291d33";
+export const STORAGE_ADDRESS = "0xE64011DEf95cdA5Bd9aFFbB073c3Ebb43Bdd1ca9";
 
 // V2 contract (TESTNET Rinkeby, DEPRECATED)
 // export const CHAIN = "Rinkeby";
@@ -16,7 +16,7 @@
 // export const STORAGE_ADDRESS = "0x4781DD29266Fbe2127088B902E566B2FF664e12a";
 
 // V2 contract (TESTNET Mumbai)
-export const CHAIN = "Mumbai";
-export const CHAIN_ID = "0x13881";
-export const CONTRACT_ADDRESS = "0x5614d79b63C0d8e8b3AC05998e7aa8708F70B414";
-export const STORAGE_ADDRESS = "0xCeD948CEB73D188E35D7D6eefE0cd95653261a11";
+// export const CHAIN = "Mumbai";
+// export const CHAIN_ID = "0x13881";
+// export const CONTRACT_ADDRESS = "0x5614d79b63C0d8e8b3AC05998e7aa8708F70B414";
+// export const STORAGE_ADDRESS = "0xCeD948CEB73D188E35D7D6eefE0cd95653261a11";
